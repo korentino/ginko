2d5293891efa1d36e5501cff6b9ac7b9ee1eb074
corentin_guyon <corentin-guyon@laposte.net>
2021-11-22 10:41:57 +0100

    pushhh

--- app.js
+++ app.js
@@ -6,6 +6,9 @@
  *                                                                      *
  ************************************************************************/
 document.addEventListener("DOMContentLoaded", function (_e) {
+    if('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('sw.js');
+      };
 
     
     /******************************************************************
